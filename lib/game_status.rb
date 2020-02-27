674ff9c27ebb24a99a600a26d17be73f321009bb
Nick Lanese <nlanese21@gmail.com>
2020-02-27 19:30:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,11 @@ WIN_COMBINATIONS = [
   [0, 1, 2], #WC[0]
   [3, 4, 5], #WC[1]
   [6, 7, 8], #WC[2]
-  
+
   [0, 3, 6], #WC[3]
   [1, 4, 7], #WC[4]
   [2, 5, 8], #WC[5]
-  
+
   [0, 4, 8], #WC[6]
   [6, 4, 2]  #WC[7]
 ]
@@ -20,10 +20,10 @@ WIN_COMBINATIONS = [
 def won?(board){
   counter = 0
   until counter == 8
-    
+
 }
 
-  
+
 def over?(board)
   if (draw(board) || won?(board))
     return true
